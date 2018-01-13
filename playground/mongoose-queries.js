bfef022fded485af90292d20e02f04e2f4056c03
George Murray <murrayge@mnstate.edu>
2018-01-13 04:10:40 -0600

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,36 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a59a33f8e04ef4b3c0fa9e8';
+// var id = '5a59cdafaad2cc0dd84befb211';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By ID', todo);
+// }).catch((e) => console.log(e));
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('ID not found');
+  }
+  console.log('User by Id', user);
+}).catch((e) => console.log(e));
