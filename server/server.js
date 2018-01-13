6ce1d7f149c1437b396791d23c2e5956066e5cad
George Murray <murrayge@mnstate.edu>
2018-01-13 01:11:57 -0600

    Add POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+
+//
+// // var newTodo = new Todo({
+// //   text: 'Cook dinner',
+// // });
+// //
+// // newTodo.save().then((doc) => {
+// //   console.log('Saved todo', doc);
+// // }, (e) => {
+// //   console.log('Unable to save todo');
+// // });
+// //
+// // var myTodo = new Todo({
+// //   text: true
+// // });
+// //
+// // myTodo.save().then((doc) => {
+// //   console.log('Saved my todo', doc);
+// // }, (e) => {
+// //   console.log('Unable to save my todo', e);
+// // });
+//
+// // User
+//
+//
+// var myUser = new User({
+//   email: 'murrayge@mnstate.edu'
+// });
+//
+// myUser.save().then((doc) => {
+//   console.log('User saved', doc);
+// }, (e) => {
+//   console.log('Unable to save', err);
+// });
