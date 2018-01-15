7e9ec16fbf16bf6151da2f886dc18ac7873a1646
George Murray <murrayge@mnstate.edu>
2018-01-14 18:20:48 -0600

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
@@ -7,7 +9,7 @@ const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
