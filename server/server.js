40be53b7ea3b99fad9d6739dfe8ccf12c43191e7
George Murray <murrayge@mnstate.edu>
2018-01-17 23:34:10 -0600

    Add GET/users/me private route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
@@ -114,6 +115,11 @@ app.post('/users', (req, res) => {
   })
 });
 
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
