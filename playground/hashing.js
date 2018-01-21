9b135160f87b2b37d9b74d5d59c75e65e589a6e8
George Murray <murrayge@mnstate.edu>
2018-01-20 18:25:11 -0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('deceoded: ', decoded);
+var hashedPassword = '$2a$10$B9JUtWXSFxmAgUKu9.v4KeIUA7I8.L2AY1Vc0sOTuvQsRf.0M1Rtq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('deceoded: ', decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 // console.log(`Message: ${message}`);
